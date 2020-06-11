a85029b47f4e76817014f196b98cfc5d28b6b3ea
Murugesan D <murugesand45@gmail.com>
2020-06-11 14:24:29 +0530

    First Commit

---
+++ Api-Gateway-Zuel/src/test/java/apigateway/main/ApiGatewayZuelApplicationTests.java
@@ -0,0 +1,13 @@
+package apigateway.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ User-Login/src/main/java/userlogin/main/UserLoginApplication.java
@@ -0,0 +1,17 @@
+package userlogin.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan(basePackages = {"userlogin.controller"})
+public class UserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginApplication.class, args);
+	}
+
+}
